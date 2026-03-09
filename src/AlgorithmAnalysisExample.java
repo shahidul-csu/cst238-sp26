329dd214dac6dc1dc9bf6f4b365c1c2736ff9ab4
Shahidul Islam <sislam@csumb.edu>
2026-03-09 11:06:52 -0700

    Week 8 Day 1

---
+++ src/AlgorithmAnalysisExample.java
@@ -0,0 +1,27 @@
+public class AlgorithmAnalysisExample {
+
+    public static void main(String[] args){
+
+    }
+
+    // O (n)
+    public static int foo(int[] data){
+        int sum = 0;
+        for(int i = 0; i < data.length; i++){
+            sum += data[i]; // sum = sum + data[i]
+        }
+        return sum * data.length;
+    }
+
+    //O (n^2)
+    public static int bar(int[] data){
+        int sum = 0;
+        for(int i = 0; i < data.length; i++){
+            for(int k = 0; k < data.length; k++){
+                sum += data[i];
+            }
+        }
+        return sum;
+    }
+}
+

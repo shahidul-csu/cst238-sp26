130412a32453ed6f08a5cd81d1e78c5803db45ee
Shahidul Islam <sislam@csumb.edu>
2026-04-06 11:07:37 -0700

    Week 11 Day 1

---
+++ src/Week11/LinkedListIntro.java
@@ -0,0 +1,18 @@
+package Week11;
+
+public class LinkedListIntro {
+    private Node head;
+
+    //To be completed in the next class
+}
+
+class Node{
+    //Payload
+    private int data;
+    //private String test;
+
+    //Reference to the next node in the Linked List
+    private Node next;
+
+    //To be completed in the next class
+}
